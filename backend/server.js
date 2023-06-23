2abfdc5aa735634c5cd53512379387329fdfab92
seokahi <skh9797@naver.com>
2023-06-23 10:30:00 +0900

    feat : 게시글 업로드 기능 추가

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,37 @@ const express = require("express");
 const app = express();const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-
+const path = require('path');
+const fs = require('fs');
+// const session = require("express-session");
+// const MySQLStore = require("express-mysql-session")(session);
+// import { useCookies } from 'react-cookie';
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 require("dotenv").config();
-const port = process.env.PORT || 3001;
 
+const port = process.env.PORT || 3001;
+const uploadDirectory = path.join(__dirname, 'uploads');
+
+// 디렉토리가 없으면 생성
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+
+// multer 설정
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDirectory);
+  },
+  filename: function (req, file, cb) {
+    const fileid = uuidv4();
+    const extension = file.originalname.split('.').pop();
+    const filename = `${fileid}.${extension}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage: storage });
 var connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
@@ -14,8 +41,25 @@ var connection = mysql.createConnection({
 })
 
 connection.connect();
+// const sessionStore = new MySQLStore({}, connection);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(
+//   session({
+//     secret: "secret key",
+//     store: sessionStore,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       domain: 'localhost',
+//       path: '/',
+//       maxAge: 24 * 6 * 60 * 10000,
+//       sameSite: 'none',
+//       httpOnly: true,
+//       secure: false,
+//     },
+//   })
+// );
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -23,6 +67,7 @@ app.use(bodyParser.json());
 app.post("/login", (req, res) => {
       const telephone = req.body.userPhone;
       const name = req.body.userName;
+      
       connection.query(
         "select * from user where userPhone=? and userName=?",
         [telephone,name],
@@ -34,7 +79,10 @@ app.post("/login", (req, res) => {
               console.log("데이터가 없습니다.");
               return res.status(404).json({ error: "데이터가 없습니다." });
             } else {
-              console.log("DB 조회 성공");
+              
+              // req.session.telephone = telephone;
+              // req.session.name = name;
+              console.log("DB 조회 성공" );
               return res.status(200).json({ success: true, data: rows });
             }
           }
@@ -45,7 +93,6 @@ app.post("/login", (req, res) => {
 app.post("/register", (req, res) => {
     const telephone = req.body.userPhone;
     const name = req.body.userName;
-    console.log("여기까지 왔니?");
     connection.query(
         "select * from user where userPhone=? and userName=?",
         [telephone,name],
@@ -76,6 +123,38 @@ app.post("/register", (req, res) => {
     );
     });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    app.post("/post", upload.single('file'), (req, res) => {
+      const telephone = req.query.telephone; 
+      console.log("여기",telephone);
+      const uploadtime = new Date().toISOString().slice(0, 19).replace("T", " ");
+      
+      // 업로드된 파일의 정보
+      const filename = req.file.filename;
+      const extension = filename.split('.').pop();
+    
+      // 새 파일 경로
+      const newfilepath = path.join(uploadDirectory, filename);
+      
+      // 파일 저장
+      fs.renameSync(req.file.path, newfilepath);
+    
+      // 데이터베이스에 URL 저장
+      const imageUrl = `/uploads/${filename}`;
+      connection.query(
+        "INSERT INTO post (userPhone, postName, postTime, imageUrl) VALUES (?, ?, ?, ?)",
+        [telephone, filename, uploadtime, imageUrl],
+        function (err, rows, fields) {
+          if (err) {
+            console.error('post 등록 실패', err);
+            return res.status(500).json({ error: "post 등록 실패" });
+          } else {
+            console.log('post 생성 성공', rows);
+            return res.status(200).json({ success: true, data: rows });
+          }
+        }
+      );
+    });
+    
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
