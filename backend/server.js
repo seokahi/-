f08ced770a69c7895232e64355c15501a5b95ff2
seokahi <skh9797@naver.com>
2023-07-04 12:28:00 +0900

    feat : 게시글 불러오기 기능 추가(node.js)

--- backend/server.js
+++ backend/server.js
@@ -123,38 +123,62 @@ app.post("/register", (req, res) => {
     );
     });
 
-    app.post("/post", upload.single('file'), (req, res) => {
-      const telephone = req.query.telephone; 
-      console.log("여기",telephone);
-      const uploadtime = new Date().toISOString().slice(0, 19).replace("T", " ");
-      
-      // 업로드된 파일의 정보
-      const filename = req.file.filename;
-      const extension = filename.split('.').pop();
-    
-      // 새 파일 경로
-      const newfilepath = path.join(uploadDirectory, filename);
-      
-      // 파일 저장
-      fs.renameSync(req.file.path, newfilepath);
-    
-      // 데이터베이스에 URL 저장
-      const imageUrl = `/uploads/${filename}`;
-      connection.query(
-        "INSERT INTO post (userPhone, postName, postTime, imageUrl) VALUES (?, ?, ?, ?)",
-        [telephone, filename, uploadtime, imageUrl],
-        function (err, rows, fields) {
-          if (err) {
-            console.error('post 등록 실패', err);
-            return res.status(500).json({ error: "post 등록 실패" });
-          } else {
-            console.log('post 생성 성공', rows);
-            return res.status(200).json({ success: true, data: rows });
-          }
+app.post("/post", upload.single('file'), (req, res) => {
+  const telephone = req.query.telephone; 
+  console.log("여기",telephone);
+  const uploadtime = new Date().toISOString().slice(0, 19).replace("T", " ");
+  
+  // 업로드된 파일의 정보
+  const filename = req.file.filename;
+  const extension = filename.split('.').pop();
+
+  // 새 파일 경로
+  const newfilepath = path.join(uploadDirectory, filename);
+  
+  // 파일 저장
+  fs.renameSync(req.file.path, newfilepath);
+
+  // 데이터베이스에 URL 저장
+  const imageUrl = `/uploads/${filename}`;
+  connection.query(
+    "INSERT INTO post (userPhone, postName, postTime, imageUrl) VALUES (?, ?, ?, ?)",
+    [telephone, filename, uploadtime, imageUrl],
+    function (err, rows, fields) {
+      if (err) {
+        console.error('post 등록 실패', err);
+        return res.status(500).json({ error: "post 등록 실패" });
+      } else {
+        console.log('post 생성 성공', rows);
+        return res.status(200).json({ success: true, data: rows });
+      }
+    }
+  );
+});
+
+app.get("/timeline", (req, res) => {
+  
+  connection.query(
+    "select * from post",
+    function (err, rows, fields) {
+      if (err) {
+        return res.status(500).json({ error: "게시글 조회 실패" });
+      } else {
+        if (rows.length === 0) {
+          console.log("데이터가 없습니다.");
+          return res.status(404).json({ error: "데이터가 없습니다." });
+        } else {
+          
+          console.log("게시글 조회 성공" );
+          return res.status(200).json({ success: true, data: rows });
         }
-      );
-    });
+      }
+    }
+  );
+});
+
+
+
     
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
